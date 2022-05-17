75f3097acf9ca2f3a1d0c34ef641652ddcd8807f
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2022-05-17 12:02:12 +0200

    added messagebroker RabbitMQ

--- src/main/java/Werkportaal/availabilityserver/controller/AvailabilityController.java
+++ src/main/java/Werkportaal/availabilityserver/controller/AvailabilityController.java
@@ -5,12 +5,13 @@ import Werkportaal.availabilityserver.service.AvailabilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/availability")
+@RequestMapping(value = "/availability", method = { RequestMethod.GET, RequestMethod.POST })
 public class AvailabilityController {
 
     @Autowired
@@ -20,7 +21,12 @@ public class AvailabilityController {
 
     @GetMapping
     public List<Availability> findAll() {
-        return availabilityService.findAll();
-    }
 
+//        String Receivedtext = "dd";
+//        availabilityService.listenForSubmittedClaims(Receivedtext);
+//        System.out.println(Receivedtext);
+
+        return availabilityService.findAll();
+
+    }
 }
